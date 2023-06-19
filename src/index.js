b81a4376d91de8b85ae9031251fb613822460bdb
Prayant-M <prayant.m@gmail.com>
2023-06-19 23:18:17 +0530

    trying to fetch data

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Show from './pages/Show';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/:id" element={<Show />} />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>  
 );
+
+// https://api.coingecko.com/api/v3/search/trending
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import homeStore from '../stores/homeStore'
 
 export default function Home() {
+  const store = homeStore()
+
+  React.useEffect(() => {
+    store.fetchCoins()
+  }, [store])
+  
   return (
     <div>
       Home
